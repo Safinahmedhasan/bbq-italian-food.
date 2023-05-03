f002c3d9825ac4833e1d79f25c31c8c5daf2d1ef
safinahmedhasan <safinahmedhaasan@gmail.com>
2023-05-03 23:03:45 +0600

    redirect fixed

--- src/component/AuthProvider/AuthProvider.jsx
+++ src/component/AuthProvider/AuthProvider.jsx
@@ -11,33 +11,41 @@ const auth = getAuth(app)
 const AuthProvider = ({children}) => {
 
     const [user , setUser] = useState(null);
+    const [loading, setLoading] = useState(true);
 
 
     const createUser = (email, password) =>{
+        setLoading(false);
         return createUserWithEmailAndPassword(auth, email, password);
+        
     }
 
     const signIn = (email, password) => {
+        setLoading(false);
         return signInWithEmailAndPassword(auth, email, password)
     }
     
     const googleSignIn = () => {
+        setLoading(false);
         const googleSignIn = new GoogleAuthProvider();
         return signInWithPopup(auth ,googleSignIn);
     }
 
     const gitHubSignIN = () =>{
+        setLoading(false);
         const gitHubSignIN = new GithubAuthProvider();
         return signInWithPopup(auth, gitHubSignIN);
     }
 
     const logOut = () =>{
+        setLoading(false);
         return signOut(auth);
     }
 
     useEffect(()=>{
      const unsubscribe = onAuthStateChanged(auth, loggedUser =>{
             setUser(loggedUser);
+            setLoading(false);
         })
         return() =>{
             unsubscribe();
@@ -48,6 +56,7 @@ const AuthProvider = ({children}) => {
     const authInfo = {
         user,  
         createUser,
+        loading,
         signIn,
         logOut,
         googleSignIn,
