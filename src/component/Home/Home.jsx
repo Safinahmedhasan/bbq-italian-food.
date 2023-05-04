f7b7e9301cf96d4e91cf9653cfdd35cb500f0e7a
safinahmedhasan <safinahmedhaasan@gmail.com>
2023-05-04 13:46:17 +0600

    new section added

--- src/component/Home/Home.jsx
+++ src/component/Home/Home.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import Banner from '../Banner/Banner';
 import MainChef from '../mainChef/MainChef';
 import AboutRestaurant from '../AboutRestaurant/AboutRestaurant';
+import FoodItem from '../FoodItem/FoodItem';
 
 const Home = () => {
     return (
@@ -9,6 +10,7 @@ const Home = () => {
             <Banner></Banner>
             <AboutRestaurant></AboutRestaurant>
             <MainChef></MainChef>
+            <FoodItem></FoodItem>
         </div>
     );
 };
